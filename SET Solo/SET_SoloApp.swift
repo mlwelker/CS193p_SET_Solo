2ca95998b7232df9562955e79c658fb5733169ac
mlwelker <nutria.watts.0g@icloud.com>
2025-01-15 18:06:44 -0600

    Initial Commit

---
+++ SET Solo/SET_SoloApp.swift
@@ -0,0 +1,17 @@
+//
+//  SET_SoloApp.swift
+//  SET Solo
+//
+//  Created by Michael Welker on 1/15/25.
+//
+
+import SwiftUI
+
+@main
+struct SET_SoloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
