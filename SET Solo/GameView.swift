4c07e2f5aec9c79b4fa0c633b89dfc10539e26b6
mlwelker <nutria.watts.0g@icloud.com>
2025-01-15 18:43:43 -0600

    add CardView

--- SET Solo/GameView.swift
+++ SET Solo/GameView.swift
@@ -2,16 +2,34 @@
 
 import SwiftUI
 
+let backgroundColor = Color(red: 60/255, green: 60/255, blue: 60/255)
+
 struct GameView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        CardView()
+            .padding()
+            .background(backgroundColor)
     }
+}
+
+struct CardView: View {
+    
+    let borderColor = Color(red: 25/255, green: 25/255, blue: 25/255)
+    
+    var body: some View {
+        let cardBase = RoundedRectangle(cornerRadius: 14)
+        ZStack {
+            cardBase.fill(.white)
+            cardBase.strokeBorder(lineWidth: 3).foregroundStyle(borderColor)
+            VStack {
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundStyle(.tint)
+                Text("Hello, world!")
+            }
+        }
+    }
+    
 }
 
 #Preview {
