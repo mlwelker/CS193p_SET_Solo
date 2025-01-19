02ec00eb3af5f7b3f8df1d4e3b9efd92047c6891
mlwelker <nutria.watts.0g@icloud.com>
2025-01-19 00:56:54 -0600

    add ShapeColor, generateDeck(), refactor

--- SET Solo/Game.swift
+++ SET Solo/Game.swift
@@ -2,37 +2,79 @@
 
 import Foundation
 
-enum ShadingType {
+enum ShadingType: CaseIterable {
     case solid
     case striped
     case open
 }
 
-enum ShapeType {
+enum ShapeColor: CaseIterable {
+    case red
+    case green
+    case purple
+}
+
+enum ShapeType: CaseIterable {
     case diamond
     case squiggle
     case oval
 }
 
-enum NumberOfSymbols: Int {
+enum NumberOfSymbols: Int, CaseIterable {
     case one = 1
     case two = 2
     case three = 3
 }
 
-struct Game<CardContent: Equatable> {
+struct Game {
     private(set) var cards: [Card]
     
     init() {
-        self.cards = []
+        self.cards = generateDeck()
+    }
+}
+
+struct Card: Equatable, Identifiable, Hashable {
+    let shadingType: ShadingType
+    let shapeColor: ShapeColor
+    let shapeType: ShapeType
+    let numberOfSymbols: NumberOfSymbols
+    
+    var isMatched: Bool
+    var id: String {
+        return "\(shadingType)-\(shapeColor)-\(shapeType)-\(numberOfSymbols)"
+    }
+}
+
+func generateTempDeck() -> [Card] {
+    var newDeck: [Card] = []
+    newDeck.append(Card(shadingType: .open, shapeColor: .green, shapeType: .diamond, numberOfSymbols: .one, isMatched: false))
+    newDeck.append(Card(shadingType: .solid, shapeColor: .red, shapeType: .oval, numberOfSymbols: .three, isMatched: false))
+    newDeck.append(Card(shadingType: .striped, shapeColor: .red, shapeType: .squiggle, numberOfSymbols: .two, isMatched: false))
+    
+    return newDeck
+}
+
+private func generateDeck() -> [Card] {
+    var newDeck: [Card] = []
+    
+    for shade in ShadingType.allCases {
+        for color in ShapeColor.allCases {
+            for shape in ShapeType.allCases {
+                for num in NumberOfSymbols.allCases {
+                    newDeck.append(
+                        Card(
+                            shadingType: shade,
+                            shapeColor: color,
+                            shapeType: shape,
+                            numberOfSymbols: num,
+                            isMatched: false
+                        )
+                    )
+                }
+            }
+        }
     }
     
-    struct Card: Equatable {
-        private let shadingType: ShadingType
-        private let shapeType: ShapeType
-        private let numberOfSymbols: NumberOfSymbols
-        
-        var isMatched: Bool
-        let content: CardContent
-    }
+    return newDeck
 }
